5518a4bfce97019dc701077e24a50612ce26ed9e
Andrii Mishchenko <maloi.promist@gmail.com>
2023-02-08 20:23:34 +0200

    - Init commit

---
+++ src/main/java/ua/kpi/mishchenko/monitoringsystem/MonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.mishchenko.monitoringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kpi/mishchenko/monitoringsystem/MonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.mishchenko.monitoringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
